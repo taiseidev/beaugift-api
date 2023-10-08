a59e0b9799f4fe75bb6f8ba709b087d6edd05838
onishi taisei <taisei.developer@gmail.com>
2023-10-09 01:27:55 +0900

    Update stripe service to create checkout session

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,6 +6,7 @@ const requiredEnvVariables = [
   'PAYPAY_CLIENT_ID',
   'PAYPAY_CLIENT_SECRET',
   'PAYPAY_MERCHANT_ID',
+  'STRIPE_SECRET_KEY',
 ];
 
 requiredEnvVariables.forEach((variable) => {
@@ -17,3 +18,4 @@ requiredEnvVariables.forEach((variable) => {
 export const PAYPAY_CLIENT_ID = process.env.PAYPAY_CLIENT_ID!;
 export const PAYPAY_CLIENT_SECRET = process.env.PAYPAY_CLIENT_SECRET!;
 export const PAYPAY_MERCHANT_ID = process.env.PAYPAY_MERCHANT_ID!;
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY!;
--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStripeDto } from './dto/create-stripe.dto';
-import { UpdateStripeDto } from './dto/update-stripe.dto';
+import { STRIPE_SECRET_KEY } from 'src/config/app.config';
+import Stripe from 'stripe';
 
 @Injectable()
 export class StripeService {
-  create(createStripeDto: CreateStripeDto) {
-    return 'This action adds a new stripe';
+  private stripe: Stripe;
+
+  constructor() {
+    this.stripe = new Stripe(STRIPE_SECRET_KEY, {
+      apiVersion: '2023-08-16',
+    });
   }
 
-  findAll() {
-    return `This action returns all stripe`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} stripe`;
-  }
-
-  update(id: number, updateStripeDto: UpdateStripeDto) {
-    return `This action updates a #${id} stripe`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stripe`;
+  async createCheckout(amount: number, currency: string) {
+    const session = await this.stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: currency || 'usd',
+            product_data: {
+              name: 'Custom product',
+            },
+            unit_amount: amount,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://uphydn.net/dokkiri/howtoplay.html',
+      cancel_url:
+        'https://www.fmworld.net/cs/azbyclub/qanavi/jsp/qacontents.jsp?PID=1504-6638',
+    });
+    return session;
   }
 }
